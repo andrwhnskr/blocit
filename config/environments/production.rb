a1627babbb36d25da7a643b93473fc372bd17f27
vagrant <vagrant@rails-dev-box>
2014-09-17 16:40:19 +0000

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Blocit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # http://hunsakerblocit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'hunsakerblocit.herokuapp.com/' }
 end
