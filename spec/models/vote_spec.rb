1cfbd17a3aeeb26730dc3ff7b467283f5e886629
vagrant <vagrant@rails-dev-box>
2014-09-25 20:58:55 +0000

    fixed rspec test for vote.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,9 +2,12 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(Vote.new(value: 1)).to_eq(true)
-        expect(Vote.new(value: -2)).to_eq(true)
-        expect(Vote.new(value: 2)).to_eq(false)
+        v = Vote.new(value: 1)
+        expect( v.valid? ).to eq true
+        v = Vote.new(value: -1)
+        expect( v.valid? ).to eq true
+        v = Vote.new(value: 2)
+        expect( v.valid? ).to eq false
       end
     end
   end
