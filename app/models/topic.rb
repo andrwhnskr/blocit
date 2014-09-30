51bb471603aec1bbd97272d709e2c80e4e95f4e4
vagrant <vagrant@rails-dev-box>
2014-09-30 22:39:50 +0000

    Attempt at private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,14 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :visible_to, -> (user) do 
+    if user
+      all
+    else
+      publicly_viewable
+    end
+  end
+  scope :privately_viewable, -> { where(public: false) }
+  
 end
