2d500aa61beba560fc4637dad7f87d2da16d8000
vagrant <vagrant@rails-dev-box>
2014-10-01 16:53:52 +0000

    Private topics fixed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -22,7 +22,7 @@
      describe "visible_to(user)" do
        it "returns all topics if the user is present" do
          user = true 
-         expect(Topic.visible_to(user)).to eq( [@public_topic && @private_topic] )
+         expect(Topic.visible_to(user)).to eq( [@public_topic, @private_topic] )
        end
  
        it "returns only public topics if user is nil" do
