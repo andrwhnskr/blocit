f353522655800e8ab0aa34a69f2b6e361376b566
vagrant <vagrant@rails-dev-box>
2014-10-02 20:15:06 +0000

    made profiles viewable publicly and refactored views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blocit::Application.routes.draw do
 
-  resources :users, only: [:update]
+  devise_for :users
+  resources :users, only: [:update, :show]
 
   resources :topics do
     resources :posts, except: [:index] 
@@ -13,8 +14,6 @@ Blocit::Application.routes.draw do
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
-
-  devise_for :users
 
   get 'about' => 'welcome#about'
 
