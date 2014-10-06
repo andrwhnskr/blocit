c0ed94dc5372aa10ee53d8ec64ad863df605dc15
vagrant <vagrant@rails-dev-box>
2014-10-06 23:06:38 +0000

    used factory girl instead of TestFactories

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ gem 'will_paginate', '~> 3.0.5'
 group :test do
   gem 'rspec-rails', '~> 3.1.0'
   gem 'capybara', '~> 2.3.0'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails', '~> 4.0'
 end
 
 # Use ActiveModel has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,11 @@
  
  describe User do
  
-   include TestFactories
  
    describe "#favorited(post)" do
     before do
-      @post = associated_post
-      @user = authenticated_user
+      @post = create(:post)
+      @user = create(:user)
       
     end
      it "returns `nil` if the user has not favorited the post" do
@@ -23,6 +22,33 @@
       expect(returned_favorite.user).to eq(@user)
       expect(returned_favorite.post).to eq(@post)
       expect(returned_favorite.id).to eq(fav.id)
+     end
+   end
+
+   describe ".top_rated" do
+ 
+     before do
+       @user1 = create(:user)
+       post = create(:post, user: @user1)
+       create(:comment, user: @user1, post: post)
+ 
+       @user2 = create(:user)
+       post = create(:post, user: @user2)
+       2.times { create(:comment, user: @user2, post: post) }
+     end
+ 
+     it "returns users ordered by comments + posts" do
+       expect( User.top_rated ).to eq([@user2, @user1])
+     end
+ 
+     it "stores a `posts_count` on user" do
+       users = User.top_rated
+       expect( users.first.posts_count ).to eq(1)
+     end
+ 
+     it "stores a `comments_count` on user" do
+       users = User.top_rated
+       expect( users.first.comments_count ).to eq(2)
      end
    end
  end
